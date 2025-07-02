59d17448deb5205d98b0b8a3b0a7faaa121e36d5
Xawier Slupik <48760619+xBlixon@users.noreply.github.com>
2025-07-02 03:35:23 +0200

    Optional working

--- fetcher.h
+++ fetcher.h
@@ -7,30 +7,48 @@ class Fetcher
 public:
     static std::optional<PersonalAccount> getPersonalAccountFromUserId(int user_id) {
         auto storage = BankDB::getStorage();
-        return storage.get_all<PersonalAccount>(
+        auto rows = storage.get_all<PersonalAccount>(
             where(c(&PersonalAccount::user_id) == user_id)
-            ).front();
+            );
+        if(rows.empty()) {
+            return std::optional<PersonalAccount>();
+        }
+        return rows.front();
+
     }
 
     static std::optional<PersonalAccount> getPersonalAccount(int id) {
         auto storage = BankDB::getStorage();
-        return storage.get_all<PersonalAccount>(
+        auto rows = storage.get_all<PersonalAccount>(
             where(c(&PersonalAccount::id) == id)
-            ).front();
+            );
+        if(rows.empty()) {
+            return std::optional<PersonalAccount>();
+        }
+        return rows.front();
+
     }
 
     static std::optional<SavingsAccount> getSavingsAccountFromUserId(int user_id) {
         auto storage = BankDB::getStorage();
-        return storage.get_all<SavingsAccount>(
+        auto rows = storage.get_all<SavingsAccount>(
             where(c(&SavingsAccount::user_id) == user_id)
-            ).front();
+            );
+        if(rows.empty()) {
+            return std::optional<SavingsAccount>();
+        }
+        return rows.front();
     }
 
     static std::optional<SavingsAccount> getSavingsAccount(int id) {
         auto storage = BankDB::getStorage();
-        return storage.get_all<SavingsAccount>(
+        auto rows = storage.get_all<SavingsAccount>(
             where(c(&SavingsAccount::id) == id)
-            ).front();
+            );
+        if(rows.empty()) {
+            return std::optional<SavingsAccount>();
+        }
+        return rows.front();
     }
 
 };
